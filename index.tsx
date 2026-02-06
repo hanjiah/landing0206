d77d5f7999d14eb12e94769539ea536aa65bfb8d
hanjiah <121755812+hanjiah@users.noreply.github.com>
2026-02-06 12:27:00 +0900

    feat: Migrate to pure HTML/JS and update dependencies
    This commit refactors the application to use pure HTML and JavaScript, removing the React dependency.
    The `index.tsx` file is no longer needed, and logic has been integrated directly into `index.html`.
    Additionally, minor updates to dependencies and CSS for animations have been made.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,3 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is no longer needed in the pure HTML/JS version.
+// All logic has been moved to index.html.
